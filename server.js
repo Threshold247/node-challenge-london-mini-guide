73cad339554f2f3dce763dd201eb638dfb5c22ea
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2022-11-15 21:00:51 +0200

    Seeting up server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
-app.get('/', (req,res) =>{
-    console.log(req.method)
-    res.send("It works")
-})
+app.get("/", (req, res) => {
+  console.log(req.method);
+  res.send("It works");
+});
 
-app.listen(port,(req,res) =>{
-    res.send(`Port is ready on ${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`Port is ready on ${port}`);
+});
